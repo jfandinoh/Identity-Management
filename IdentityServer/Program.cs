e7bfa35f1baa38a9d5577810074f09ac2d3cf6ef
Jaime Andrés Fandiño Herrera <jfandinoh@hotmail.com>
2022-12-18 16:57:11 +1100

    Setup identityServer

---
+++ IdentityServer/Program.cs
@@ -0,0 +1,27 @@
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Test;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+
+    options.EmitStaticAudienceClaim = true;
+})
+    .AddTestUsers(new List<TestUser>())
+    .AddInMemoryClients(new List<Client>())
+    .AddInMemoryApiResources(new List<ApiResource>())
+    .AddInMemoryApiScopes(new List<ApiScope>())
+    .AddInMemoryIdentityResources(new List<IdentityResource>());
+
+var app = builder.Build();
+
+app.UseIdentityServer();
+
+app.MapGet("/", () => "Identity Server!");
+
+app.Run();
